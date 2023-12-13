2c151500d4cbda0a3561f0bfa55a1053b409741a
Carlos Mertens <mertenscarlos3000@gmail.com>
2023-12-13 23:56:56 +0100

    Create ExpandableText component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { Header } from './components/Header';
 import { Divider } from './components/Divider/Divider';
+import ExpandableText from './components/ExpandableText';
 import Info from './components/Info';
 import ListGroup from './components/ListGroup/ListGroup';
 import Like from './components/Like';
@@ -9,6 +10,31 @@ import './App.css';
 
 function App() {
   const [count, setCount] = useState(0);
+  const [tags, setTags] = useState(['happy', 'cheerfull']);
+  const [customer, setCustomer] = useState({
+    name: 'Carlos',
+    address: {
+      city: 'Berlin',
+      zipCode: 0,
+    },
+  });
+
+  const handleUnmutable = () => {
+    setCustomer({
+      ...customer,
+      address: { ...customer.address, zipCode: Math.floor(Math.random() * 10) },
+    });
+
+    // add
+    setTags([...tags, 'funny']);
+
+    // update
+    // setTags(tags.map((tag) => (tag === 'happy' ? 'happiness' : tag)));
+
+    // remove
+    // setTags(tags.filter((tag) => tag !== 'cheerfull'));
+  };
+  console.log({ tags });
 
   const cities = ['London', 'New York', 'Berlin', 'Paris', 'Madrid'];
   const conferences = ['JS', 'Typescript', 'React', 'NextJS'];
@@ -22,12 +48,18 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+        <br />
+        <br />
+        <button onClick={handleUnmutable}>
+          ZipCode is {customer.address.zipCode}
+        </button>
         <Info>
           Edit <code>src/App.tsx</code> and save to test HMR
         </Info>
       </div>
       <Info>Click on the Vite and React logos to learn more</Info>
 
+      <Divider />
       <Like onClick={() => console.log('clicked')} />
 
       <Divider />
@@ -43,6 +75,25 @@ function App() {
           heading='Dev Conference'
           onSelectItem={handleSelectItem}
         />
+      </section>
+
+      <Divider />
+      <section>
+        <h2>Expandable Text</h2>
+        <ExpandableText>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Numquam,
+          exercitationem reiciendis. Voluptatum esse cum possimus cupiditate
+          tenetur voluptas? Dolore beatae officia voluptates nihil saepe atque,
+          commodi velit, aperiam vel quod autem dignissimos aspernatur
+          voluptatum quisquam, reiciendis ipsa laboriosam ratione ab facilis
+          dolor dicta et voluptate. Magni, eius sequi dolor odio ab corporis
+          cupiditate mollitia ducimus facere reprehenderit pariatur doloribus,
+          velit quas. Libero earum magnam eum doloribus. Saepe aliquam odit
+          laboriosam inventore quae ratione id autem a illum, expedita omnis
+          deleniti praesentium commodi cupiditate repellat pariatur, debitis
+          distinctio fugit sint porro consequuntur molestias ut? Quo blanditiis
+          est possimus, minima voluptates soluta.
+        </ExpandableText>
       </section>
     </>
   );
--- src/components/Like.tsx
+++ src/components/Like.tsx
@@ -8,6 +8,20 @@ interface Props {
 const Like = ({ onClick }: Props) => {
   const [status, setStatus] = useState(true);
 
+  // TODO:
+  // Refactor componentDidMount into useEffect
+  // componentDidMount() {
+  //   const balance = localStorage.getItem('balance');
+  //   if (balance) {
+  //     this.setState({
+  //       balance: Number(balance),
+  //     });
+  //   }
+  //   window.addEventListener('beforeunload', () => {
+  //     localStorage.setItem('balance', this.state.balance);
+  //   });
+  // }
+
   const toggle = () => {
     setStatus(!status);
     onClick();
